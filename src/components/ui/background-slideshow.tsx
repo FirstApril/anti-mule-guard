29488540757638fc7e988f63c42939f5e1f137d6
FirstApril <jedsadapronnene46@gmail.com>
2025-08-17 10:10:03 +0700

    Update background-slideshow.tsx

--- src/components/ui/background-slideshow.tsx
+++ src/components/ui/background-slideshow.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import bgSlide1 from '@/assets/grandmacriedwithsmile_.png';
+import bgSlide1 from '@/assets/grandmacriedwithsmile_-1.png';
 import bgSlide2 from '@/assets/bg-slide-1.jpg';
 import bgSlide3 from '@/assets/bg-slide-1.jpg';
 
